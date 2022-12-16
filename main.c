ebb35e7647adfdd6bc3927b57fc8d6cd0d94979e
ConnorBP <connor.postma@gmail.com>
2022-12-16 14:25:29 -0500

    added file license and removed linux shellcode

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 // Self Modifying code example
 // based on https://shanetully.com/2013/12/writing-a-self-mutating-x86_64-c-program/
+// License: MIT
 
 #include <stdio.h>
 #include <windows.h>
@@ -10,18 +11,6 @@ void foo2(void);
 void bar2(void);
 void print_function_instructions(void *func_ptr, size_t func_len);
 void print_function_shellcode(void *func_ptr, size_t func_len);
-
-// char shellcode[] =
-//     "\x48\x31\xd2"                              // xor    %rdx, %rdx
-//     "\x48\x31\xc0"                              // xor    %rax, %rax
-//     "\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00"  // mov    $0x68732f6e69622f, %rbx
-//     "\x53"                                      // push   %rbx
-//     "\x48\x89\xe7"                              // mov    %rsp, %rdi
-//     "\x50"                                      // push   %rax
-//     "\x57"                                      // push   %rdi
-//     "\x48\x89\xe6"                              // mov    %rsp, %rsi
-//     "\xb0\x3b"                                  // mov    $0x3b, %al
-//     "\x0f\x05";                                 // syscall
 
 char shellcode[] =
 "\x55"
